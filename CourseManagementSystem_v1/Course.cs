1396679ee91cfbbadeb8e7b653c20d5fa6678a0c
UT03211 <UT03211@UNICOMTIC17>
2024-10-11 14:15:57 +0530

    Add project files.

---
+++ CourseManagementSystem_v1/Course.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v1
+{
+    internal class Course
+    {
+        private string courseId { get; set; }
+        private string title { get; set; }
+        private string Duration { get; set; }
+        private decimal Price { get; set; }
+
+        public Course(string courseId, string title, string duration, decimal price)
+        {
+            this.courseId = courseId;
+            this.title = title;
+            Duration = duration;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {courseId}, Title: {title}, Duration: {Duration}, Price: {Price}";
+        }
+    }
+}
---
+++ CourseManagementSystem_v1/CourseManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v1
+{
+    internal class CourseManager
+    {
+        private List<Course> courses = new List<Course>();
+
+        //add course
+        public void CreateCourse()
+        {
+            Console.WriteLine("Enter course Id for add a course: ");
+            string id =(Console.ReadLine()??"1");
+            Console.WriteLine("enter course title for the course: ");
+            string title =(Console.ReadLine()??"");
+            Console.WriteLine("enter a duration for a course: ");
+            string duration = (Console.ReadLine() ?? "");
+            Console.WriteLine();
+        }
+
+    }
+}
